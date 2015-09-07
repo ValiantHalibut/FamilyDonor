199cb3683df6bc6ece487cab5554f0119b2de928
Thomas <Thomas@192.168.1.100>
2015-09-07 15:02:27 -0400

    Setting up Phalcon skeleton

--- family/public/index.php
+++ family/public/index.php
@@ -1,8 +1,4 @@
 <?php
 echo "This is family/public/index.php";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+define ("APP_PATH", realpath("..") . "/");
+echo "<br/>" . APP_PATH;
