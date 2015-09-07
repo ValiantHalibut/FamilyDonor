92234bc7634f4aefa25526aa6c0dce10cadeb752
Thomas <Thomas@192.168.1.100>
2015-09-07 15:28:52 -0400

    Setting up Phalcon skeleton

--- family/public/index.php
+++ family/public/index.php
@@ -2,3 +2,10 @@
 echo "This is family/public/index.php";
 define ("APP_PATH", realpath("..") . "/");
 echo "<br/>" . APP_PATH;
+
+
+use Phalcon\Config\Adapter\Ini as ConfigIni;
+
+
+$config = new ConfigIni(APP_PATH . "app/config/config.ini");
+echo $config;
