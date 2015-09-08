63d3d3ae42a1451be2cf01e590252b9c1e7b58b1
Thomas <Thomas@192.168.1.100>
2015-09-07 23:06:36 -0400

    Setting up Phalcon skeleton

--- family/app/plugins/SecurityPlugin.php
+++ family/app/plugins/SecurityPlugin.php
@@ -6,8 +6,18 @@ use Phalcon\Mvc\Dispatcher;
 
 class SecurityPlugin extends Plugin
 {
-    public function beforeDispatch(Event $event, Dispatcher $dispatch)
+    public function beforeDispatch(Event $event, Dispatcher $dispatcher)
     {
-        return true;
+        $controller = $dispatcher->getControllerName();
+        $action = $dispatcher->getActionName();
+        
+        $allowed = true;
+        
+        if($allowed) {
+            $dispatcher->forward(array(
+                "controller"    => $controller,
+                "action"         => $action
+            ));
+        }
     }
 }
