4fc0326b2616e536be33a1b2c81581652d2efea3
Thomas <Thomas@192.168.1.100>
2015-09-11 22:20:01 -0400

    Setting up Phalcon skeleton

--- family/app/controllers/IndexController.php
+++ family/app/controllers/IndexController.php
@@ -2,8 +2,11 @@
 
 class IndexController extends ControllerBase
 {
-    public function indexAction()
+    public function indexAction($logData)
     {
-        
+        if($logData) {
+            $failNote = "<p class='logFail'>Login failed because: " . $logData . ". Please try again.</p>";
+            $this->view->setVar("failNote", $failNote);
+        }
     }
 }
